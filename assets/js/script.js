0b329d0aff034bf8f16885bcc9ff1c153f448806
Lisa Reitinger <kzml8pbb@students.codeinstitute.net>
2024-11-28 12:10:05 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     }
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -80,6 +82,8 @@ function calculateCorrecrAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplement operator ${operator}`);
         throw `Unimplement operator ${operator}.Aborting!`;
@@ -114,7 +118,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 
 }
 
